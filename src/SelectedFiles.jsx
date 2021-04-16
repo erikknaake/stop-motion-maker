3b6e620c25ebbe2f498c5681fbc1e45db8c8b886
Erik Knaake <e.knaake@student.han.nl>
2021-04-16 20:54:10 +0200

    remove image prerender

--- src/SelectedFiles.jsx
+++ src/SelectedFiles.jsx
@@ -2,13 +2,6 @@ import React from 'react';
 
 export const SelectedFiles = ({images}) => (
     <div className="flex">
-        {images.map(image => {
-            return (
-                <div key={image.name} className="image-preview">
-                    <img src={URL.createObjectURL(image)} className="image-preview__thumbnail" alt=""/>
-                    <p className="image-preview__image-name">{image.name}</p>
-                </div>
-            );
-        })}
+        Selected {images.length} files
     </div>
 )
